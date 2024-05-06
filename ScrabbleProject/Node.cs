ec9543f8017cc71d696bb16616b8c7f93ebc9548
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2024-05-06 18:27:22 -0500

    Moved code over to the scrabble project.

--- ScrabbleProject/Node.cs
+++ ScrabbleProject/Node.cs
@@ -2,23 +2,50 @@ using System;
 using System.Collections.Generic;
 
 [Serializable]
-public class Node
+public class Node : IEquatable<Node>
 {
     public bool IsSuccessState { get; set; } = false;
 
+    // Denotes the Character on the edge leading into the node
+    //public char ParentChar {get; set;}
+    public List<char> ParentChars { get; set; } = [];
+    private readonly char[] charSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    // Denotes the char that the transition from the parent is on.
     public Dictionary<char, Node> Children { get; set; } = new Dictionary<char, Node>();
 
-
-    public void AddChild(Node newChild, char letter)
+    public bool Equals(Node? other)
     {
-        try
+        if (other == null)
         {
-            Children.Add(letter, newChild);
+            return false;
         }
-        catch (ArgumentException)
+        if (other.ParentChars != ParentChars)
         {
-            Console.WriteLine("An element is already mapped to letter: " + letter);
+            return false;
         }
+        string mySubtree = findSubtreeString(this);
+        string otherSubtree = findSubtreeString(other);
+        bool comparison = mySubtree.Equals(otherSubtree);
+        return comparison;
     }
+    private string findSubtreeString(Node root)
+    {
+        string result = "";
+        foreach (char c in charSet)
+        {
+            if (root.Children.TryGetValue(c, out Node? value))
+            {
 
+                foreach (char character in value.ParentChars)
+                {
+                    result += character;
+                }
+                findSubtreeString(value);
+            }
+        }
+        return result;
+    }
+    public override bool Equals(object? obj) => Equals(obj as Node);
+    public override int GetHashCode() => (IsSuccessState, ParentChars).GetHashCode();
 }
